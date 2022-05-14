67034ae62455910e8487b1d7bdb441d59d33ef39
sumbat15 <sumbat15@gmail.com>
2022-05-14 18:52:40 +0300

    poppins

--- theme/index.js
+++ theme/index.js
@@ -2,8 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    // heading: "Poppins, sans-serif",
-    // body: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif",
+    body: "Poppins, sans-serif",
   },
   colors: {
     blue: {
